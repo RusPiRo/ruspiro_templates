ab305844d669d58e75a0fc42487274ffe934dacd
2ndTaleStudio <43264484+2ndTaleStudio@users.noreply.github.com>
2020-09-15 23:08:23 +0200

    fix version placeholder

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@
  * Author: {{authors}}
  * License: Apache License 2.0 / MIT
  **********************************************************************************************************************/
-#![doc(html_root_url = "https://docs.rs/{{project-name}}/{{version}}")]
+#![doc(html_root_url = "https://docs.rs/{{project-name}}/||VERSION||")]
 // we require to run with 'std' in unit tests and doc tests to have an allocator in place
 #![cfg_attr(not(any(test, doctest)), no_std)]
 
